b8fd8a81ddbaa3c16ad67d498a55cd888235d24a
Jikaoro <j.orozbaeva@gmail.com>
2021-07-19 15:42:09 -0500

    added github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -2,4 +2,7 @@ public class GitCool {
     // B23 Is learning  GIT
     // B23 is making some sense now
     // B23 is a special number
+
+    // we have added our github account under inteliJ
+    // make sure to click apply and click on to save
 }
