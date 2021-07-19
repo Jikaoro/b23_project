479ed1debaa4b43ce321e8e229143593ba0b6d79
Jikaoro <j.orozbaeva@gmail.com>
2021-07-19 15:44:16 -0500

    added github account again

--- src/GitCool.java
+++ src/GitCool.java
@@ -5,4 +5,8 @@ public class GitCool {
 
     // we have added our github account under inteliJ
     // make sure to click apply and click on to save
+    // we were able to succesfully share a project under our acoount
+    // it combines the 3 actions below
+    // create remote repository under your account
+    // Push local commits to remote repo
 }
